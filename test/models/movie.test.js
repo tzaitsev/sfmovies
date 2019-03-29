d11d93f5863bb3a8079f18ffc6ccc55b2c57fe11
Tim Zaitsev <tzaitsev@gmail.com>
2019-03-29 15:15:03 -0700

    feat(tests): add model tests and ensure coverage (#4)

---
+++ test/models/movie.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Movie = require('../../lib/models/movie');
+
+describe('movie model', () => {
+
+  describe('serialize', () => {
+
+    it('includes all of the necessary fields', () => {
+      const movie = Movie.forge();
+
+      expect(movie.serialize()).to.have.all.keys([
+        'id',
+        'title',
+        'release_year',
+        'object'
+      ]);
+    });
+
+  });
+
+});
