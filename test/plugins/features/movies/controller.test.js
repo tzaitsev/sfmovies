ae7c160849bea493b6798aa39fa2ada4e9f0045b
Tim Zaitsev <tim@lob.com>
2019-04-02 11:32:23 -0700

    feat(get): movies by filter

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -7,11 +7,64 @@ describe('movie controller', () => {
   describe('create', () => {
 
     it('creates a movie', async () => {
-      const payload = { title: 'WooTMovie!' };
-
+      const payload = { title: 'WooTMovies!' };
       const movie = await Controller.create(payload);
-
       expect(movie.get('title')).to.eql(payload.title);
+    });
+
+  });
+
+  describe('get', () => {
+
+    it('returns a list of movies if no query params are passed', async () => {
+      const movies = await Controller.findAll();
+      expect(movies.models['0'].attributes.title).to.eql('180');
+      expect(movies.models['1'].attributes.title).to.eql('24 Hours on Craigslist');
+      expect(movies.models.length).to.be.above(1);
+    });
+
+    it('returns a list of one movie if it is an exact title match to a query parameter', async () => {
+      const movies = await Controller.findAll({ title: '180' });
+      expect(movies.models['0'].attributes.title).to.eql('180');
+      expect(movies.models.length).to.equal(1);
+    });
+
+    it('returns a specific movie if it is an exact release_year match to a query parameter', async () => {
+      const movies = await Controller.findAll({ release_year: '1915' });
+      expect(movies.models['0'].attributes.title).to.eql('A Jitney Elopement');
+      expect(movies.models.length).to.equal(1);
+    });
+
+    it('returns a list of movies with a fuzzy title', async () => {
+      const movies = await Controller.findAll({ release_year: '1947', title: '%p%' });
+      expect(movies.models['0'].attributes.title).to.eql('Dark Passage');
+      expect(movies.models['1'].attributes.title).to.eql('Nora Prentiss');
+      expect(movies.models.length).to.equal(2);
+    });
+
+    it('returns a list of movies with a start_year', async () => {
+      const movies = await Controller.findAll({ start_year: 2015 });
+      expect(movies.models['0'].attributes.title).to.eql('Age of Adaline');
+      expect(movies.models.length).to.be.above(1);
+    });
+
+    it('returns a list of movies with an end_year', async () => {
+      const movies = await Controller.findAll({ end_year: 1915 });
+      expect(movies.models['0'].attributes.title).to.eql('A Jitney Elopement');
+      expect(movies.models.length).to.equal(1);
+    });
+
+    it('returns a list of movies with a release_year', async () => {
+      const movies = await Controller.findAll({ release_year: 1915 });
+      expect(movies.models['0'].attributes.title).to.eql('A Jitney Elopement');
+      expect(movies.models.length).to.equal(1);
+    });
+
+    it('returns a list of movies with a start_year and end_year', async () => {
+      const movies = await Controller.findAll({ start_year: 1927, end_year: 1935 });
+      expect(movies.models['0'].attributes.title).to.eql('Barbary Coast');
+      expect(movies.models['1'].attributes.title).to.eql('The Jazz Singer');
+      expect(movies.models.length).to.equal(2);
     });
 
   });
--- test/plugins/features/movies/index.test.js
+++ test/plugins/features/movies/index.test.js
@@ -7,7 +7,7 @@ describe('movies integration', () => {
   describe('create', () => {
 
     it('creates a movie', async () => {
-      const payload = { title: 'Le_film' };
+      const payload = { title: 'Le_Test_Movie' };
 
       const response = await Movies.inject({
         url: '/movies',
@@ -18,7 +18,114 @@ describe('movies integration', () => {
       expect(response.statusCode).to.eql(200);
       expect(response.result.object).to.eql('movie');
       expect(response.result.title).to.eql(payload.title);
-      expect(response.result.release_year).to.be.null;
+      expect(response.result.release_year).to.be.a('null');
+    });
+
+  });
+
+  describe('get', () => {
+
+    it('gets a list of movies', async () => {
+      const response = await Movies.inject({
+        url: '/movies',
+        method: 'GET'
+      });
+
+      expect(response.statusCode).to.eql(200);
+      expect(response.result['0'].title).to.eql('180');
+      expect(response.result['1'].title).to.eql('24 Hours on Craigslist');
+      expect(response.result.length).to.be.above(2);
+    });
+
+    it('gets a specific movie', async () => {
+      const response = await Movies.inject({
+        url: '/movies?title=180',
+        method: 'GET'
+      });
+
+      expect(response.statusCode).to.eql(200);
+      expect(response.result['0'].title).to.eql('180');
+      expect(response.result.length).to.equal(1);
+
+    });
+
+    it('supports fuzzy titles', async () => {
+      const response = await Movies.inject({
+        url: '/movies?title=A Jitney%',
+        method: 'GET'
+      });
+
+      expect(response.statusCode).to.eql(200);
+      expect(response.result['0'].title).to.eql('A Jitney Elopement');
+      expect(response.result.length).to.equal(1);
+    });
+
+    it('supports fuzzy titles and release years', async () => {
+      const response = await Movies.inject({
+        url: '/movies?title=%p%&release_year=1947',
+        method: 'GET'
+      });
+
+      expect(response.statusCode).to.eql(200);
+      expect(response.result['0'].title).to.eql('Dark Passage');
+      expect(response.result['1'].title).to.eql('Nora Prentiss');
+      expect(response.result.length).to.equal(2);
+    });
+
+    it('supports release years', async () => {
+      const response = await Movies.inject({
+        url: '/movies?release_year=1915',
+        method: 'GET'
+      });
+
+      expect(response.statusCode).to.eql(200);
+      expect(response.result['0'].title).to.eql('A Jitney Elopement');
+      expect(response.result.length).to.equal(1);
+    });
+
+    it('supports start_year', async () => {
+      const response = await Movies.inject({
+        url: '/movies?start_year=2015',
+        method: 'GET'
+      });
+
+      expect(response.statusCode).to.eql(200);
+      expect(response.result['0'].title).to.eql('Age of Adaline');
+      expect(response.result.length).to.be.above(1);
+    });
+
+    it('supports end_year', async () => {
+      const response = await Movies.inject({
+        url: '/movies?end_year=1915',
+        method: 'GET'
+      });
+
+      expect(response.statusCode).to.eql(200);
+      expect(response.result['0'].title).to.eql('A Jitney Elopement');
+      expect(response.result.length).to.equal(1);
+    });
+
+    it('supports start_year and end_year', async () => {
+      const response = await Movies.inject({
+        url: '/movies?start_year=1927&end_year=1935',
+        method: 'GET'
+      });
+
+      expect(response.statusCode).to.eql(200);
+      expect(response.result['0'].title).to.eql('Barbary Coast');
+      expect(response.result['1'].title).to.eql('The Jazz Singer');
+      expect(response.result.length).to.equal(2);
+    });
+
+    it('supports release_year and start_year and end_year ', async () => {
+      const response = await Movies.inject({
+        url: '/movies?release_year=1915&start_year=1927&end_year=1935',
+        method: 'GET'
+      });
+
+      expect(response.statusCode).to.eql(200);
+      expect(response.result['0'].title).to.eql('A Jitney Elopement');
+      expect(response.result.length).to.equal(1);
 
     });
 
